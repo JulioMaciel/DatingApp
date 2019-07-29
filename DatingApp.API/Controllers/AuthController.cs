4c75afbaf81ac10ef045912d8866ceccf105aa84
Julio Maciel <juketo@gmail.com>
2019-07-29 18:48:06 +1000

    End Section 11: Expanded Register Form

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -35,14 +35,15 @@ namespace DatingApp.API.Controllers
             if (await _repo.UserExists(userForRegisterDto.Username))
                 return BadRequest("Username already exists");
 
-            var userToCreate = new User
-            {
-                Username = userForRegisterDto.Username
-            };
+            // var userToCreate = new User { Username = userForRegisterDto.Username };
+            var userToCreate = _mapper.Map<User>(userForRegisterDto);
 
             var createdUser = await _repo.Register(userToCreate, userForRegisterDto.Password);
 
-            return StatusCode(201); // brb
+            var userToReturn = _mapper.Map<UserForDetailedDto>(createdUser);
+
+            // return StatusCode(201); // brb
+            return CreatedAtRoute("GetUser", new { controller = "Users", Id = createdUser.Id }, userToReturn);
         }
 
         [HttpPost("login")]
